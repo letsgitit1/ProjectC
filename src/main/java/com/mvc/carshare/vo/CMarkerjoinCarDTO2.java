7367534b54db8ef9cf2367c481d813d341165531
letsgitit1 <moonnetworkqq@Gmail.com>
2023-04-07 18:59:04 +0900

    맵에 찜 수정,  맵에 판매하기 잘찍히게 수정

--- src/main/java/com/mvc/carshare/vo/CMarkerjoinCarDTO2.java
+++ src/main/java/com/mvc/carshare/vo/CMarkerjoinCarDTO2.java
@@ -8,14 +8,14 @@ public class CMarkerjoinCarDTO2 {
 	 private double latitude;
 	 private double longitude;
 	 
-	 private int car_id;
+	 private int cno;
 	 private String car_image;
 	 private String car_model;
 	 
 	 
-	 private int m_id;
+	 private int mno;
 	 
-	 private int r_id;
+	 private int id;
 	 private int rent_price;
 	 private int rent_time;
 	 
